1fd260adea5e55638ae933b68834928742118d53
John Sanders <oigewan@gmail.com>
2014-11-10 00:26:36 +1300

    Remove active record since we don't need a DB

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root to: 'home#index'
+  root to: 'home#index'
+  get 'battle' => 'home#battle'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
